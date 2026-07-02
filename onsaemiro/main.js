c0be43f2548a853ce63eaf8712289fc87e5d7860
yezineo <yezzinieo@gmail.com>
2026-07-02 14:50:45 +0900

    Add files via upload

---
+++ onsaemiro/main.js
@@ -0,0 +1,32 @@
+// 햄버거 메뉴
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('is-open');
+    menuBtn.classList.toggle('is-open');
+});
+
+// 기타농산물 - 슬라이더
+const swiper = new Swiper('.g-slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 10,
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+    pagination: {
+        el: '.products-grid .swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-direction .swiper-button-next',
+        prevEl: '.swiper-direction .swiper-button-prev',
+    },
+});
