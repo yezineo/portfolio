d3913dbf49e760171a219737ab1bc218133124e7
yezineo <yezzinieo@gmail.com>
2026-07-06 16:20:31 +0900

    Update main.js

--- onsaemiro/main.js
+++ onsaemiro/main.js
@@ -31,6 +31,26 @@ const swiper = new Swiper('.g-slider', {
     },
 });
 
+// 리뷰 섹션 - 슬라이더
+const reviewSwiper = new Swiper('.r-slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 4000,                      // 4초마다 넘김
+        disableOnInteraction: false,      // 조작해도 자동재생 유지
+        pauseOnMouseEnter: true,          // 마우스 올리면 멈춤
+    },
+    pagination: {
+        el: '.review-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.review-wrap-next',
+        prevEl: '.review-wrap-prev',
+    },
+});
+
 // aos 스크립트 라이브러리
 AOS.init({
     duration: 1000,
